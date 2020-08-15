26236676665a91086cfa019b3cb33f5cb6d9719d
Bhavin Agarwal <bhavib.789@gmail.com>
2020-08-15 17:20:32 +0530

    Import Home component asynchronously

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+
+import loadable from "@loadable/component";
+const HomeComponent = loadable(() => import("./Home"));
 
 const App = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
+    <Route exact path="/" component={HomeComponent} />
   </Switch>
 );
 
